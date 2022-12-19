c31c9ae8e35e9d74f4e5a6831c0b8bea8f1a4aea
rsl150101 <dnwnd501@naver.com>
2022-12-20 02:54:52 +0900

    API Router 설정, DB 연결

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const express = require("express");
+const db = require("./models/index");
+
+const todosRouter = require("./routes/todos.router.js");
 
 const app = express();
-const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.send("Hi!");
-});
-app.use("/api", express.json(), router);
+app.use("/api", express.json(), todosRouter);
+app.use(express.static("./assets"));
 
 app.listen(8080, () => {
   console.log("서버가 켜졌어요!");
