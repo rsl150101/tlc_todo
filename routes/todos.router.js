0a19de0ead54a422ba896c8f0a3ecbe82fc20c70
rsl150101 <dnwnd501@naver.com>
2022-12-21 03:59:27 +0900

    to-do 등록, 순서 바꾸기 구현

--- routes/todos.router.js
+++ routes/todos.router.js
@@ -11,3 +11,32 @@ router.post("/todos", async (req, res) => {
   await todo.save();
   res.send({ todo });
 });
+
+router.get("/todos", async (req, res) => {
+  const todos = await Todo.find().sort("-order").exec();
+
+  res.send({ todos });
+});
+
+router.patch("/todos/:todoId", async (req, res) => {
+  const { todoId } = req.params;
+  const { order } = req.body;
+
+  const currentTodo = await Todo.findById(todoId);
+  if (!currentTodo) {
+    throw new Error("존재하지 않는 todo 데이터입니다.");
+  }
+
+  if (order) {
+    const targetTodo = await Todo.findOne({ order }).exec();
+    if (targetTodo) {
+      targetTodo.order = currentTodo.order;
+      await targetTodo.save();
+    }
+    currentTodo.order = order;
+  }
+
+  await currentTodo.save();
+
+  res.send({});
+});
