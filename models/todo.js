add088bddee92db5bd8344825aa739fe1d06b73e
rsl150101 <dnwnd501@naver.com>
2022-12-20 18:08:24 +0900

    todo 모델 추가, todo 등록 기능 구현

---
+++ models/todo.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+const TodoSchema = new mongoose.Schema({
+  value: String,
+  doneAt: Date,
+  order: Number,
+});
+TodoSchema.virtual("todoId").get(function () {
+  return this._id.toHexString();
+});
+TodoSchema.set("toJSON", {
+  virtuals: true,
+});
+module.exports = mongoose.model("Todo", TodoSchema);
--- routes/todos.router.js
+++ routes/todos.router.js
@@ -1,8 +1,13 @@
-const express = require("express");
-const router = express.Router();
+const Todo = require("./models/todo");
 
-router.get("/", (req, res) => {
-  res.send("Hi!");
+router.post("/todos", async (req, res) => {
+  const { value } = req.body;
+  const maxOrderByUserId = await Todo.findOne().sort("-order").exec();
+  /* exec() 를 사용하면 유사 Promise 가 아닌 온전한 Promise 를 반환값으로 얻을 수 있다.
+     에러가 났을 때 stack trace 에 오류가 발생한 코드의 위치가 포함되기 때문에 권장한다.
+     참조 : https://tesseractjh.tistory.com/166 */
+  const order = maxOrderByUserId ? maxOrderByUserId.order + 1 : 1;
+  const todo = new Todo({ value, order });
+  await todo.save();
+  res.send({ todo });
 });
-
-module.exports = router;
